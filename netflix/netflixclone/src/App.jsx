d1204ff606f623f1615854a7e7a065b790c5f343
Victor rodrigues da rocha <89555576+victorrochaaudaces@users.noreply.github.com>
2022-01-26 17:21:35 -0300

    continue deploy

--- netflix/netflixclone/src/App.jsx
+++ netflix/netflixclone/src/App.jsx
@@ -1,20 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import "./App.css";
 
 function APP() {
+  const [movieList, setMovieList] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     // colocamos função asyn para esperar o resultado e evitar erros noi console
     const loadAll = async () => {
       // pegando a lista total
       let list = await Tmdb.getHomeList();
-      console.log(list)
-    }
+      setMovieList(list);
+    };
 
     loadAll();
-  }, [])
+  }, []);
 
-  return <div>Olá mundo!</div>;
+  return (
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  );
 }
 
 export default APP;
